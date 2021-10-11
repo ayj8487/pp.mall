e3f4e670b48b698d7b492831b7fd5d089a89aab4
ayj84 <ayj84@DESKTOP-9BVNURB>
2021-10-12 03:47:05 +0900

    pp.mall firstCommit

---
+++ kubg/src/main/java/com/kubg/domain/MemberVO.java
@@ -0,0 +1,96 @@
+package com.kubg.domain;
+
+import java.util.Date;
+
+public class MemberVO {
+	
+/* 
+com.kubg.domain은 VO(Value Object) 패키지. 주로 사용하는 데이터 타입을 저장
+com.kubg.persistence는 DAO(Data Access Object) 패키지. 데이터 베이스와 접속하는 역할
+com.kubg.serevice는 Service 패키지. DAO와 Controller 사이를 연결해주는 역할
+ */
+	
+
+	/*
+	 * create table tbl_member (
+    userId      varchar2(50)    not null,
+    userPass    varchar2(100)   not null,
+    userName    varchar2(30)    not null,
+    userPhon    varchar2(20)    not null,
+    userAddr1   varchar2(20)    null,
+    userAddr2   varchar2(50)    null,
+    userAddr3   varchar2(50)    null,
+    regiDate    date            default sysdate,
+    verify      number          default 0,
+    primary key(userId)
+	);
+	 */
+	
+	private String userId;
+	private String userPass;
+	private String userName;
+	private String userPhon;
+	private String userAddr1;
+	private String userAddr2;
+	private String userAddr3;
+	private Date regiDate;
+	private int verify;
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPass() {
+		return userPass;
+	}
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPhon() {
+		return userPhon;
+	}
+	public void setUserPhon(String userPhon) {
+		this.userPhon = userPhon;
+	}
+	public String getUserAddr1() {
+		return userAddr1;
+	}
+	public void setUserAddr1(String userAddr1) {
+		this.userAddr1 = userAddr1;
+	}
+	public String getUserAddr2() {
+		return userAddr2;
+	}
+	public void setUserAddr2(String userAddr2) {
+		this.userAddr2 = userAddr2;
+	}
+	public String getUserAddr3() {
+		return userAddr3;
+	}
+	public void setUserAddr3(String userAddr3) {
+		this.userAddr3 = userAddr3;
+	}
+	public Date getRegiDate() {
+		return regiDate;
+	}
+	public void setRegiDate(Date regiDate) {
+		this.regiDate = regiDate;
+	}
+	public int getVerify() {
+		return verify;
+	}
+	public void setVerify(int verify) {
+		this.verify = verify;
+	}
+	
+	
+}
